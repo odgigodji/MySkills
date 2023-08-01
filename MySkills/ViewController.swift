9a319ab03a8d69b8f567774420f4cc271f730a3d
nikita evdokimov <evdokimovnikita1@gmail.com>
2023-08-01 12:02:38 +0300

    start

---
+++ MySkills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySkills
+//
+//  Created by Nikita Evdokimov on 01.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
